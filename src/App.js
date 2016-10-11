53e7d2971e1381401262e9e6351b3139bf41ec1b
Rahul Gaba <rahul_gaba@external.mckinsey.com>
2016-10-12 03:29:40 +0530

    feed component route fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.scss';
-import Feed from './components/Feed/Feed'
+import Feed from './components/Feed/Feed.jsx'
 import Header from './components/Header/Header'
 
 class App extends Component {
